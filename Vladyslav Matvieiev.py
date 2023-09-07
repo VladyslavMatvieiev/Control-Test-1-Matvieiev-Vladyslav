2bf2e1ba1660981b526d02a47c3ceb326570fee1
VladyslavMatvieiev <144101929+VladyslavMatvieiev@users.noreply.github.com>
2023-09-07 19:28:26 +0300

    Update Vladyslav Matvieiev.py

--- Vladyslav Matvieiev.py
+++ Vladyslav Matvieiev.py
@@ -1,13 +1,22 @@
-import random
+from random import randint
+
 array1 = []
 array2 = []
-summ = []
-for _ in range(10):
-    array1.append(random.randint(0, 9))
-    array2.append(random.randint(0, 9))
+array3 = []
+
 for i in range(10):
-    summa = array1[i] + array2[i]
-    summ.append(summa)
-print("Перший масив:", array1)
-print("Другий масив:", array2)
-print("Третій мавсив:", summ[::-1]) 
+    array1.append(randint(0, 9))
+    array2.append(randint(0, 9))
+
+carry = 0
+for i in range(9, -1, -1):
+    total = array1[i] + array2[i] + carry
+    carry = total // 10
+    array3.insert(0, total % 10)
+
+if carry > 0:
+    array3.insert(0, carry)
+
+print(f"Перший масив: {array1}")
+print(f"Другий масив:{array2}")
+print(f"сума: {array3}")
