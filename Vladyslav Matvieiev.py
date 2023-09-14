5226a4e1a49e4e0c0865c3d6615ad5839f4921b4
VladyslavMatvieiev <144101929+VladyslavMatvieiev@users.noreply.github.com>
2023-09-14 10:04:27 +0300

    Update Vladyslav Matvieiev.py

--- Vladyslav Matvieiev.py
+++ Vladyslav Matvieiev.py
@@ -1,22 +1,36 @@
 from random import randint
 
-array1 = []
-array2 = []
-array3 = []
+def fill_array_with_random_numbers(size):
+    return [randint(1, 9) for _ in range(size)]
 
-for i in range(10):
-    array1.append(randint(0, 9))
-    array2.append(randint(0, 9))
+def add_arrays_with_carry(first_array, second_array):
+    max_size = max(len(first_array), len(second_array))
+    result_array = []
+    temp = 0
 
-carry = 0
-for i in range(9, -1, -1):
-    total = array1[i] + array2[i] + carry
-    carry = total // 10
-    array3.insert(0, total % 10)
+    for i in range(max_size):
+        first_digit = first_array[-i - 1] if i < len(first_array) else 0
+        second_digit = second_array[-i - 1] if i < len(second_array) else 0
+        total = first_digit + second_digit + temp
+        temp = total // 10
+        result_array.insert(0, total % 10)
+    while len(first_array) < len(second_array):
+        first_array.insert(0, 0)
+    while len(second_array) < len(first_array):
+        second_array.insert(0, 0)
+    if temp > 0:
+        result_array.insert(0, temp)
 
-if carry > 0:
-    array3.insert(0, carry)
+    return result_array
 
-print(f"Перший масив: {array1}")
-print(f"Другий масив:{array2}")
-print(f"сума: {array3}")
+first_arr_size = int(input("Enter size of 1 arr: "))
+second_arr_size = int(input("Enter size of 2 arr: "))
+
+first_array = fill_array_with_random_numbers(first_arr_size)
+second_array = fill_array_with_random_numbers(second_arr_size)
+
+result_array = add_arrays_with_carry(first_array, second_array)
+
+print(f"First array: {first_array}")
+print(f"Second array: {second_array}")
+print(f"Result: {result_array}")
